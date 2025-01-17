9e548e4d51a5a2a6b3ac7d8b9ed24bfdd6e34f85
Ansar Ali <ansaralyh@gmail.com>
2025-01-17 14:12:40 +0500

    for github streak

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,3 +1,4 @@
 import { Request, Response } from "express";
 import { createUser } from "../zodValidations/userValidation";
 import userModel from '../models/userModel'
+import { Express } from "express";
