5b11770190d4ed7fde7037644e478751df2041ac
Ansar Ali <ansaralyh@gmail.com>
2025-01-22 05:41:44 +0500

    chanegs

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,3 +2,4 @@ import { Request, Response } from "express";
 import { createUser } from "../zodValidations/userValidation";
 import userModel from '../models/userModel'
 import { Express } from "express";
+ 
