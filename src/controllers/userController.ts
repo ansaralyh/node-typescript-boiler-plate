9b8034ad49ef20d54d115aaf3ef8c12c9f26ec90
Ansar Ali <ansaralyh@gmail.com>
2025-01-22 21:46:59 +0500

    for streak

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express";
 import { createUser } from "../zodValidations/userValidation";
-import userModel from '../models/userModel'
+import userModel from '../models/userModel';
 import { Express } from "express";
+
+
+
+
+
+
+
+
+  
  
